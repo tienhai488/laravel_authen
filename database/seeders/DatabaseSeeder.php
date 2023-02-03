0e1e9897b24f94a1ef48fd51898a448b88ce2c63
tienhai488 <97999852+tienhai488@users.noreply.github.com>
2023-02-03 08:59:05 +0700

    add data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +16,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        
+        $groupId = DB::table('groups')->insertGetId([
+            'name'=>'Admin',
+            'user_id'=>0,
+            'created_at'=>date('Y-m-d H:i:s'),
+            'updated_at'=>date('Y-m-d H:i:s'),
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        
+        if($groupId>0){
+            $userId = DB::table('users')->insertGetId([
+                'name'=>'TienHai',
+                'group_id'=>$groupId,
+                'email'=>'tienhai488@gmail.com',
+                'password'=>Hash::make('12345678'),
+                'user_id'=>0,
+                'created_at'=>date('Y-m-d H:i:s'),
+                'updated_at'=>date('Y-m-d H:i:s'),
+            ]);
+            
+            DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+            if($userId>0){
+                DB::table('posts')->insertGetId([
+                    'title'=>"Bai viet thu 1",
+                    'content'=>"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                    'user_id'=>$userId,
+                    'created_at'=>date('Y-m-d H:i:s'),
+                    'updated_at'=>date('Y-m-d H:i:s'),
+                ]);
+            }
+        }
     }
 }
